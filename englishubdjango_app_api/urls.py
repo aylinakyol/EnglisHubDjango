38fd08e97dfd9940540d9d2c984e4f7c7270c030
Aylin Akyol <akyolaylin2@gmail.com>
2025-06-01 12:32:53 +0000

    get methodları ve url leri eklendi

--- englishubdjango_app_api/urls.py
+++ englishubdjango_app_api/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
+from englishubdjango_app_api import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('get-types/', views.get_types),
+    path('get-levels/', views.get_levels),
+    path('get-questions/', views.get_questions),
+    path('get-options/', views.get_options),
 ]
--- englishubdjango_app_api/views.py
+++ englishubdjango_app_api/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from django.http import JsonResponse # type: ignore
+from englishubdjango_app_api.models import *
+import traceback
+from django.conf import settings # type: ignore
+import json
 
-# Create your views here.
+def get_types(request):
+    try:
+        types = Type.objects.all()
+        data = [
+            {
+                "id": type.id,
+                "name": type.name,
+            }
+            for type in types
+        ]
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        # DEBUG modunda detaylı hata mesajı göster
+        if settings.DEBUG:
+            message = (
+                f"***** ---> str(e): {str(e)} "
+                f"***** ---> e.args: {str(e.args)} "
+                f"***** ---> type(e): {type(e).__name__} "
+                f"***** ---> traceback: {traceback.format_exc()}"
+            )
+        else:
+            message = "An unexpected error occurred. Please contact support."
+        return JsonResponse({"error": message}, status=500)
+    
+def get_levels(request):
+    try:
+        levels = Level.objects.all()
+        data = [
+            {
+                "id": level.id,
+                "name": level.name,
+            }
+            for level in levels
+        ]
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        # DEBUG modunda detaylı hata mesajı göster
+        if settings.DEBUG:
+            message = (
+                f"***** ---> str(e): {str(e)} "
+                f"***** ---> e.args: {str(e.args)} "
+                f"***** ---> type(e): {type(e).__name__} "
+                f"***** ---> traceback: {traceback.format_exc()}"
+            )
+        else:
+            message = "An unexpected error occurred. Please contact support."
+        return JsonResponse({"error": message}, status=500)
+
+def get_questions(request):
+    try:
+        questions = Question.objects.all()
+        data = [
+            {
+                "id": question.id,
+                "description": question.description,
+                "level": question.level.name,
+            }
+            for question in questions
+        ]
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        # DEBUG modunda detaylı hata mesajı göster
+        if settings.DEBUG:
+            message = (
+                f"***** ---> str(e): {str(e)} "
+                f"***** ---> e.args: {str(e.args)} "
+                f"***** ---> type(e): {type(e).__name__} "
+                f"***** ---> traceback: {traceback.format_exc()}"
+            )
+        else:
+            message = "An unexpected error occurred. Please contact support."
+        return JsonResponse({"error": message}, status=500)
+    
+def get_options(request):
+    try:
+        options = Option.objects.all()
+        data = [
+            {
+                "id": option.id,
+                "description": option.description,
+                "question": option.question.description,
+            }
+            for option in options
+        ]
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        # DEBUG modunda detaylı hata mesajı göster
+        if settings.DEBUG:
+            message = (
+                f"***** ---> str(e): {str(e)} "
+                f"***** ---> e.args: {str(e.args)} "
+                f"***** ---> type(e): {type(e).__name__} "
+                f"***** ---> traceback: {traceback.format_exc()}"
+            )
+        else:
+            message = "An unexpected error occurred. Please contact support."
+        return JsonResponse({"error": message}, status=500)
